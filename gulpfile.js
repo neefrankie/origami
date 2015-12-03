336b8196c01bfcddcec0f56a5c634f675d723c46
weiguo.ni <neefrankie@gmail.com>
2015-12-03 18:05:17 +0800

    integrate ftc-icons

--- gulpfile.js
+++ gulpfile.js
@@ -3,58 +3,30 @@ const gulp = require('gulp');
 const del = require('del');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
-
-const svgmin = require('gulp-svgmin');
-const svgToCss = require('gulp-svg-to-css');
-const svgstore = require('gulp-svgstore');
-
-const svg2png = require('gulp-svg2png');
-const rsvg = require('gulp-rsvg');
-
+const sass = require('gulp-sass');
 const sequence = require('gulp-sequence');
 
 const browserSync = require('browser-sync').create();
 
-//Generate inline svg css
-gulp.task('svg2css', function() {
-  return gulp.src('demo/svg/*.svg')
-    .pipe(svgmin())
-    .pipe(svgToCss({
-      name: 'icons.data.svg.css',
-      prefix: 'icon-',
-      template: '.{{prefix}}{{filename}}{background-image:url("{{{dataurl}}}");}'
+gulp.task('wiredep', () => {
+  gulp.src('demo/styles/*.scss')
+    .pipe(wiredep({
+      ignorePath: /^(\.\.\/)+/
     }))
-    .pipe(gulp.dest('demo/styles/'));
-});
+    .pipe(gulp.dest('demo/styles'));
 
-//Generate a svg sprite with `symbol` elements
-gulp.task('svgstore', function() {
-  var svgs = gulp.src('demo/svg/*.svg')
-    .pipe(svgmin())
-    .pipe(svgstore())
-    .pipe(rename('sprite.symbol.svg'))
-    .pipe(gulp.dest('demo/icons/'));
-});
-
-gulp.task('svgmin', function() {
-  return gulp.src('demo/svg/*.svg')
-    .pipe(svgmin())
-    .pipe(gulp.dest('demo/icons/svg'));
-});
-
-gulp.task('svg2png', function() {
-  return gulp.src('demo/svg/*.svg')
-    .pipe(svg2png([0.2]))
-    .pipe(gulp.dest('demo/icons/png'));
-});
-
-gulp.task('rsvg', function() {
-  return gulp.src('demo/svg/social*.svg')
-    .pipe(rsvg({
-      format: 'png',
-      scale: 0.2
+  gulp.src('demo/*.html')
+    .pipe(wiredep({
+      ignorePath: /^(\.\.\/)*\.\./
     }))
-    .pipe(gulp.dest('demo/icons/png'));
+    .pipe(gulp.dest('demo'));
+});
+
+gulp.task('sass', function() {
+  return gulp.src('demo/styles/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('.tmp/styles'))
+    .pipe(browserSync.stream());
 });
 
 //distribute
@@ -70,20 +42,24 @@ gulp.task('dist', ['clean'], function() {
     .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('build', ['svg2css', 'svgstore', 'svgmin', 'dist']);
-
-gulp.task('serve', ['build'], function() {
+gulp.task('serve', function() {
   browserSync.init({
-    files: ['demo/**/*', 'dist/*'],
     server: {
-      baseDir: ['demo', 'src'],
+      baseDir: ['.tmp', 'demo', 'src'],
       routes: {
         '/bower_components': 'bower_components'
       }
     }
   });
 
+  gulp.watch([
+    'demo/*.html',
+    'src/*.js',
+    '.tmp/styles/*.css'
+  ]).on('change', browserSync.reload);
+
   gulp.watch('src/*', ['dist']);
-  gulp.watch(['demo/*.html', 'demo/styles', 'dist']).on('change', browserSync.reload);
+  gulp.watch(['demo/styles/*.scss'], ['sass']);
 });
+
 
