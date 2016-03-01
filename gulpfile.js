aeada0cd9e1bdb598829fa4caad9e6e6f9d86a5c
weiguo.ni <neefrankie@gmail.com>
2016-03-01 16:50:27 +0800

    unify build process; add deploy task

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,103 @@
-'use strict'
+var path = require('path');
 var gulp = require('gulp');
-var del = require('del');
 var $ = require('gulp-load-plugins')();
-
+var del = require('del');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var browserify = require('browserify');
 var watchify = require('watchify');
 var debowerify = require('debowerify');
 var babelify = require('babelify');
-
-var bourbon = require('bourbon').includePaths;
+var cssnext = require('postcss-cssnext');
 var browserSync = require('browser-sync').create();
 
+var config = require('./config.json');
+var projectName = path.basename(__dirname);
 
-gulp.task('styles', function() {
+gulp.task(function mustache() {
+  const DEST = '.tmp';
+  const options = {
+    "shareItem": [
+      {
+        "icon": "wechat",
+        "name": "微信"
+      },
+      {
+        "icon": "weibo",
+        "name": "微博"
+      },
+      {
+        "icon": "linkedin",
+        "name": "领英"
+      },
+      {
+        "icon": "facebook",
+        "name": "Facebook"
+      },
+      {
+        "icon": "twitter",
+        "name": "Twitter"
+      },
+      {
+        "icon": "url",
+        "name": "复制链接"
+      }
+    ]
+  }
+
+  return gulp.src('demo/index.mustache')
+    .pipe($.changed(DEST))
+    .pipe($.mustache(options, {
+      extension: '.html'
+    }))
+    .pipe(gulp.dest(DEST))
+    .pipe(browserSync.stream({once: true}));
+});
+
+gulp.task('styles', function styles() {
+  const DEST = '.tmp/styles';
+
   return gulp.src('demo/main.scss')
+    .pipe($.changed(DEST))
     .pipe($.plumber())
-    .pipe($.sourcemaps.init())
+    .pipe($.sourcemaps.init({loadMaps:true}))
     .pipe($.sass({
       outputStyle: 'expanded',
       precision: 10,
-      includePaths: ['bower_components', bourbon]
+      includePaths: ['bower_components']
     }).on('error', $.sass.logError))
+    .pipe($.postcss([
+      cssnext({
+        features: {
+          colorRgba: false
+        }
+      })
+    ]))
     .pipe($.sourcemaps.write('./'))
-    .pipe(gulp.dest('.tmp'))
-    .pipe(browserSync.stream());
+    .pipe(gulp.dest(DEST))
+    .pipe(browserSync.stream({once: true}));
 });
 
-gulp.task('scripts', function() {
+/* Bundle js with watchify + browserify + debowerify + babelify*/
+gulp.task('scripts', function scripts() {
   var b = browserify({
     entries: 'demo/main.js',
     debug: true,
     cache: {},
     packageCache: {},
-    transform: [debowerify, babelify],
+    transform: [babelify, debowerify],
     plugin: [watchify]
   });
 
   b.on('update', bundle);
   b.on('log', $.util.log);
 
-  return bundle();
+  bundle();
 
   function bundle(ids) {
     $.util.log('Compiling JS...');
     if (ids) {
-      console.log('Chnaged Files:\n' + ids);
+      console.log('Changed Files:\n' + ids);
     }   
     return b.bundle()
       .on('error', function(err) {
@@ -58,27 +109,55 @@ gulp.task('scripts', function() {
       .pipe(buffer())
       .pipe($.sourcemaps.init({loadMaps: true}))
       .pipe($.sourcemaps.write('./'))
-      .pipe(gulp.dest('.tmp'))
+      .pipe(gulp.dest('.tmp/scripts'))
       .pipe(browserSync.stream({once:true}));
   }
+});
+
+gulp.task(function js() {
+  const DEST = '.tmp/scripts';
+
+  var b = browserify({
+    entries: 'demo/main.js',
+    debug: true,
+    cache: {},
+    packageCache: {},
+    transform: [babelify, debowerify]
+  });
+
+  return b.bundle()
+    .on('error', function(err) {
+      $.util.log(err.message);
+      this.emit('end')
+    })
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe($.sourcemaps.init({loadMaps: true}))
+    .pipe($.sourcemaps.write('./'))
+    .pipe(gulp.dest(DEST));
 });
 
 gulp.task('clean', function() {
   return del(['.tmp/**']);
 });
 
-gulp.task('serve', gulp.series(gulp.parallel('styles', 'scripts'), function erve () {
+gulp.task('serve', gulp.parallel('mustache', 'styles', 'scripts', function erve () {
   browserSync.init({
     server: {
-      baseDir: ['.tmp', 'demo'],
+      baseDir: ['.tmp'],
       routes: {
         '/bower_components': 'bower_components'
       }
     }
   });
 
-  gulp.watch(['demo/*.html'], browserSync.reload);
+  gulp.watch('demo/*.mustache', gulp.parallel('mustache'));
 
-  gulp.watch(['demo/main.scss', 'src/**/*.scss'], gulp.parallel('styles'));
+  gulp.watch(['demo/*.scss', 'src/**/*.scss'], gulp.parallel('styles'));
 
+}));
+
+gulp.task('deploy', gulp.series('clean', 'mustache', 'styles', 'js', function() {
+  return gulp.src('.tmp/**/*')
+    .pipe(gulp.dest(config.deploy.assets + projectName));
 }));
