fffc65fefe7de9e77596144dc48098a1ddc36839
neefrankie <neefrankie@gmail.com>
2016-09-18 17:38:45 +0800

    add helper

---
+++ helper.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const nunjucks = require('nunjucks');
+
+var env = new nunjucks.Environment(
+  new nunjucks.FileSystemLoader(
+    ['partials', 'demos/src'], 
+    {noCache: true}
+  ),
+  {autoescape: false}
+);
+
+function render(name, context) {
+  const obj = {};
+  return new Promise(function(resolve, reject) {
+    env.render(name, context, function(err, result) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+function readJson(filename) {
+  return new Promise(
+    function(resolve, reject) {
+      fs.readFile(filename, 'utf8', function(err, data) {
+        if (err) {
+          console.log('Cannot find file: ' + filename);
+          reject(err);
+        } else {
+          resolve(JSON.parse(data));
+        }
+      });
+    }
+  );
+}
+
+function readFile(filename) {
+  return new Promise(
+    function(resolve, reject) {
+      fs.readFile(filename, 'utf8', function(err, data) {
+        if (err) {
+          console.log('Cannot find file: ' + filename);
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
+    }
+  );
+}
+
+module.exports = {
+  readJson: readJson,
+  readFile: readFile,
+  render: render
+};
