ca10f170d47202f20beee2d533c00782959d4cd2
neefrankie <neefrankie@gmail.com>
2016-10-06 10:09:33 +0800

    copy favicons

--- gulpfile.js
+++ gulpfile.js
@@ -129,7 +129,7 @@ gulp.task('serve', gulp.parallel('html', 'styles', () => {
 gulp.task('copy', () => {
   const DEST = path.resolve(__dirname, demosDir, projectName);
   console.log(`Deploying to ${DEST}`);
-  return gulp.src(['.tmp/**/*', 'static*/**/*.{svg,png}',])
+  return gulp.src(['.tmp/**/*', 'static*/**/*.{svg,png,ico}',])
     .pipe(gulp.dest(DEST));
 });
 
