4afeff1bf8852bb6966f2a494e7c13c72a85770a
neefrankie <neefrankie@gmail.com>
2016-10-06 18:52:18 +0800

    generate href for partials

---
+++ demos/src/data.js
@@ -0,0 +1,7 @@
+const shareItems = require('./share-items');
+const config = {
+  title: "FTC share components",
+  url: "http://interactive.ftchinese.com/components/ftc-share.html",
+  summary: "This is the demo page for ftc share component"
+}
+module.exports = shareItems(config);
