1b4a070871c189093ac9600a8a246b720f5314cb
weiguo.ni <neefrankie@gmail.com>
2016-12-26 18:08:34 +0800

    simplify demo structure

--- demos/src/data.js
+++
@@ -1,7 +0,0 @@
-const shareItems = require('./share-items');
-const config = {
-  title: "FTC share components",
-  url: "http://interactive.ftchinese.com/components/ftc-share.html",
-  summary: "This is the demo page for ftc share component"
-}
-module.exports = shareItems(config);
