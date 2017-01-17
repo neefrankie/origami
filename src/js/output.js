494f9412cfca1c8947bc67d5f0f184f6d093d33d
weiguo.ni <neefrankie@gmail.com>
2017-01-17 15:19:31 +0800

    add svgo

--- src/js/output.js
+++ src/js/output.js
@@ -1,22 +1,26 @@
 const fs = require('mz/fs');
+const SVGO = require('svgo');
+const svgo = new SVGO();
 const svg2png = require('./svg2png.js');
 
 function output(svg, to) {
 	console.log(`Generating ${to}.svg`);
 
-	fs.writeFile(`${to}.svg`, svg, 'utf8')
-		.catch(err => {
-			console.log(err);
-		})
+	svgo.optimize(svg, (result) => {
+		fs.writeFile(`${to}.svg`, result.data, 'utf8')
+			.catch(err => {
+				console.log(err);
+			})
 
-	svg2png(svg)
-		.then(buffer => {
-			console.log(`Generating ${to}.png`);
-			return fs.writeFile(`${to}.png`, buffer)
-		})
-		.catch(err => {
-			console.log(err);
-		});	
+		svg2png(result.data)
+			.then(buffer => {
+				console.log(`Generating ${to}.png`);
+				return fs.writeFile(`${to}.png`, buffer)
+			})
+			.catch(err => {
+				console.log(err);
+			});			
+	});
 }
 
 module.exports = output;
