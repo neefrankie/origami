adde0f10e7c6cadfb0b8364abc47e368c97c2b3f
weiguo.ni <neefrankie@gmail.com>
2017-03-31 16:06:55 +0800

    add junk

--- gulpfile.js
+++ gulpfile.js
@@ -1,100 +1,93 @@
-const fs = require('mz/fs');
+const pify = require('pify');
+const fs = require('fs-jetpack');
 const path = require('path');
-const co = require('co');
-const nunjucks = require('nunjucks');
-const env = new nunjucks.Environment(
-  new nunjucks.FileSystemLoader(
-    [process.cwd()],
-    {noCache: true, }
-  ),
-  {autoescape: false}
-);
-
-function render(template, context, destName) {
-  return new Promise(function(resolve, reject) {
-    env.render(template, context, function(err, result) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve({
-          name: destName,
-          content: result
-        });
-      }
-    });
-  });
-}
-const _ = require('lodash');
-const mkdirp = require('./lib/mkdir.js');
-
+const loadJsonFile = require('load-json-file');
+const inline = pify(require('inline-source'));
 const del = require('del');
 const browserSync = require('browser-sync').create();
 const cssnext = require('postcss-cssnext');
-
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')();
 
-const settings = require('./src/js/settings.json');
-const logos = settings.map(setting => {
-  return setting.themes.map(theme => {
-    return theme.type === 'default' ? setting.name : `${setting.name}-${theme.type}`;
-  });
+const nunjucks = require('nunjucks');
+nunjucks.configure(process.cwd(), {
+  noCache: true,
+  watch: false
 });
+const render = pify(nunjucks.render);
+const junk = require('junk');
 
-const demosDir = '../ft-interact/demos';
-const projectName = path.basename(__dirname);
+const fav = require('./lib/fav.js');
+const logoImages = require('./lib/index.js');
+
+const svgDir = path.resolve(__dirname, 'svg');
+const targetDir = path.resolve(__dirname, '../ft-interact');
+const demoDir = `${targetDir}/demos`;
+const project = path.basename(__dirname);
+
+process.env.NODE_ENV = 'development';
 
 // change NODE_ENV between tasks.
-gulp.task('prod', function(done) {
-  process.env.NODE_ENV = 'prod';
-  done();
+gulp.task('prod', function() {
+  return Promise.resolve(process.env.NODE_ENV = 'production');
 });
 
-gulp.task('dev', function(done) {
-  process.env.NODE_ENV = 'dev';
-  done();
+gulp.task('dev', function() {
+  return Promise.resolve(process.env.NODE_ENV = 'development');
 });
+
+function buildPage(template, context) {
+  return render(template, context)
+    .then(html => {
+      if (process.env.NODE_ENV === 'production') {
+        return inline(html, {
+          compress: true,
+          rootpath: path.resolve(process.cwd(), '.tmp')
+        });
+      }    
+      return html;      
+    })
+    .catch(err => {
+      throw err;
+    });
+}
 
 // /* demo tasks */
-gulp.task('html', () => {
-// determine whether include `/api/resize-iframe.js` listed in `ftc-components`.
-  var embedded = process.env.NODE_ENV === 'prod';
+gulp.task('html', async () => {
+  const env = {
+    isProduction: process.env.NODE_ENV === 'production'
+  };
 
-  return co(function *() {
-    const destDir = '.tmp';
+  try {
+    const [json, filenames] = await Promise.all([
+      loadJsonFile('origami.json'),
+      fs.listAsync(svgDir)
+    ]);
 
-    yield mkdirp(destDir);
+    const logos = filenames.filter(junk.not).map(name => {
+      return path.basename(name, '.svg');
+    });
 
-    const origami = yield fs.readFile('origami.json', 'utf8');
+    const promisedAction = json.demos.map(demo => {
+      const context = Object.assign(demo, {logos, env});
+      return buildPage(demo.template, context)  
+        .then(html => {
+          return fs.writeAsync(`.tmp/${demo.name}.html`, html);
+        });
+    });
 
-    const demos = JSON.parse(origami).demos;
-
-    const renderResults = yield Promise.all(demos.map(function(demo) {
-
-      const context = Object.assign({}, demo, {
-        logos: _.flatten(logos),
-        embedded
-      });
-      return render(demo.template, context, demo.name);
-    }));
-
-    yield Promise.all(renderResults.map(result => {
-      const dest = `.tmp/${result.name}.html`;
-      return fs.writeFile(dest, result.content, 'utf8');
-    }));
-  })
-  .then(function(){
+    await promisedAction;
     browserSync.reload('*.html');
-  }, function(err) {
-    console.error(err.stack);
-  });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 gulp.task('styles', function styles() {
-  const DEST = '.tmp/styles';
+  const dest = '.tmp/styles';
 
   return gulp.src('demos/src/demo.scss')
-    .pipe($.changed(DEST))
+    .pipe($.changed(dest))
     .pipe($.plumber())
     .pipe($.sourcemaps.init({loadMaps:true}))
     .pipe($.sass({
@@ -110,20 +103,30 @@ gulp.task('styles', function styles() {
       })
     ]))
     .pipe($.sourcemaps.write('./'))
-    .pipe(gulp.dest(DEST))
+    .pipe(gulp.dest(dest))
     .pipe(browserSync.stream({once: true}));
 });
 
-gulp.task('clean', function() {
+gulp.task('images', () => {
+  return Promise.all([
+    logoImages(),
+    fav({htmlTo: path.resolve(process.cwd(), 'public/favicons')})
+  ])
+  .catch(err => {
+    console.log(err);
+  })
+});
+
+gulp.task('clean', () => {
   return del(['.tmp/**']).then(()=>{
     console.log('Old files deleted');
   });
 });
 
-gulp.task('serve', gulp.parallel('html', 'styles', () => {
+gulp.task('serve', gulp.parallel('images', 'html', 'styles', () => {
   browserSync.init({
     server: {
-      baseDir: ['.tmp', 'dist'],
+      baseDir: ['.tmp', 'public'],
       directory: true,
       routes: {
         '/dist': 'dist'
@@ -140,10 +143,12 @@ gulp.task('serve', gulp.parallel('html', 'styles', () => {
     gulp.parallel('styles')
   );
 
+  gulp.watch('svg/*.svg', gulp.parallel('images'));
+
 }));
 
 gulp.task('copy', () => {
-  const DEST = path.resolve(__dirname, demosDir, projectName);
+  const dest = `${demosDir}/${project}`;
   console.log(`Deploying to ${DEST}`);
   return gulp.src(['.tmp/**/*', 'dist*/**/*.{svg,png,ico}',])
     .pipe(gulp.dest(DEST));
@@ -152,6 +157,14 @@ gulp.task('copy', () => {
 gulp.task('demo', gulp.series('clean', 'prod', gulp.parallel('html', 'styles'), 'copy', 'dev'));
 
 gulp.task('deploy', () => {
-  return gulp.src('dist/*')
-    .pipe(gulp.dest(path.revole(__dirname, '../ft-interact/bower_components/ftc-logos/')))
+  return Promise.all([
+      logoImages(`${targetDir}/${project}`),
+      fav({
+        imageTo: `${targetDir}/favicons`,
+        htmlTo: null
+      })
+    ])
+    .catch(err => {
+      console.log(err);
+    });
 });
