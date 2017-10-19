acc7a6196782bb5bfd69571ff0192877143ecfbd
weiguo.ni <neefrankie@gmail.com>
2017-10-20 00:27:33 +0800

    try to generate dist files

--- gulpfile.js
+++ gulpfile.js
@@ -1,133 +1,51 @@
-const pify = require('pify');
 const path = require('path');
-const fs = require('fs-jetpack');
 const loadJsonFile = require('load-json-file');
-const inline = pify(require('inline-source'));
-const nunjucks = require('nunjucks');
-nunjucks.configure(process.cwd(), {
-  noCache: true,
-  watch: false
-});
-const render = pify(nunjucks.render);
 const stats = require('@ftchinese/component-stats');
 
 const browserSync = require('browser-sync').create();
-const cssnext = require('postcss-cssnext');
 const gulp = require('gulp');
-const $ = require('gulp-load-plugins')();
-
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
 const getFooterData = require('./lib/index.js');
 
 const deployDir = path.resolve(__dirname, '../ft-interact');
 const demoDir = `${deployDir}/demos/${path.basename(__dirname)}`;
 
-process.env.NODE_ENV = 'development';
-
 // change NODE_ENV between tasks.
 gulp.task('prod', function(done) {
-  process.env.NODE_ENV = 'production';
-  done();
+  return Promise.resolve(process.env.NODE_ENV = 'production');
 });
 
 gulp.task('dev', function(done) {
-  process.env.NODE_ENV = 'development';
-  done();
+  return Promise.resolve(process.env.NODE_ENV = 'development');
 });
-
-function buildPage(data) {
-  const env = {
-    isProduction: process.env.NODE_ENV === 'production'
-  };
-  const context = Object.assign(data, {env});
-  const dest = path.resolve(process.cwd(), `.tmp/${data.name}.html`);
-
-  return render(data.template, context)
-    .then(html => {
-      if (process.env.NODE_ENV === 'production') {
-        console.log('Inlining source')
-        return inline(html, {
-          compress: true,
-          rootpath: path.resolve(process.cwd(), '.tmp')
-        });
-      }    
-      return Promise.resolve(html);
-    })
-    .then(html => {
-      return fs.writeAsync(dest, html);
-    })
-    .catch(err => {
-      throw err;
-    });
-}
 
 gulp.task('html', async function () {
 
-  return loadJsonFile('origami.json')
-    .then(json => {
-      return Promise.all(json.demos.map(demo => {
-
-        const data = Object.assign(demo, {
-          footer: getFooterData({
-            theme: demo.theme,
-            type: demo.type
-          })
-        });
-               
-        return buildPage(data);
-      })); 
-    })
-    .then(() => {
-      browserSync.reload('*.html');
-      return Promise.resolve();
-    })
-    .catch(err => {
-      console.log(err);
+  const json = await loadJsonFile('origami.json');
+  await Promise.all(json.demos.map(async demo => {
+    const data = Object.assign(demo, {
+      footer: getFooterData(demo.theme)
     });
+    await buildPage(data);
+  }));
 
-  try {
-    const json = await loadJsonFile('origami.json');
-
-    const promisedAction = json.demos.map(demo => {
-      const context = Object.assign(demo, {
-        footer: getFooterData({
-          theme: demo.theme,
-          type: demo.type
-        })
-      });
-      return buildPage(demo.template, context)  
-        .then(html => {
-          return fs.writeAsync(`.tmp/${demo.name}.html`, html);
-        });
-    });
-
-    await promisedAction;
-    browserSync.reload('*.html');
-  } catch (e) {
-    console.log(e);
-  }
+  browserSync.reload('*.html');
 });
 
 gulp.task('styles', function styles() {
-  const DEST = '.tmp/styles';
-
   return gulp.src('demos/src/*.scss')
-    .pipe($.changed(DEST))
-    .pipe($.plumber())
-    .pipe($.sourcemaps.init({loadMaps:true}))
-    .pipe($.sass({
+    .pipe(sourcemaps.init({loadMaps:true}))
+    .pipe(sass({
       outputStyle: 'expanded',
       precision: 10,
       includePaths: ['bower_components']
-    }).on('error', $.sass.logError))
-    .pipe($.postcss([
-      cssnext({
-        features: {
-          colorRgba: false
-        }
-      })
-    ]))
-    .pipe($.sourcemaps.write('./'))
-    .pipe(gulp.dest(DEST))
+    }))
+    .on('error', err => {
+      console.log(err)
+    })
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist/styles'))
     .pipe(browserSync.stream({once:true}));
 });
 
@@ -137,7 +55,7 @@ gulp.task('serve',
     function serve() {
     browserSync.init({
       server: {
-        baseDir: ['.tmp'],
+        baseDir: ['dist'],
         index: 'footer.html',
         directory: true,
         routes: {
@@ -161,11 +79,3 @@ gulp.task('stats', () => {
       console.log(err);
     });
 });
-
-gulp.task('copy', () => {
-  console.log(`Copy demo to ${demoDir}`);
-  return gulp.src('.tmp/*.html')
-    .pipe(gulp.dest(demoDir));
-});
-
-gulp.task('demo', gulp.series('prod', 'styles', 'html', 'stats', 'copy', 'dev'));
