eac11e22f73584edbf28cf84bbcad8f9a1845ff9
wangyichen1064431086 <1064431086@qq.com>
2017-12-20 17:26:58 +0800

    Has built the structure of header html and data

--- src/js/header.js
+++ src/js/header.js
@@ -1,67 +1,58 @@
-import search from './search';
-import mega from './mega';
-import drawer from './drawer';
-import subnav from './subnav';
-import sticky from './sticky';
+// import header需要的一系列子模块吧
 
 class Header {
 	constructor (headerEl) {
 		/**
-		 * @param headerEl:Type string或为空.Type string,Eg:'[data-o-component]="o-header"]'
+		 * @param headerEl: Type String or 为空 or  HTMLElement
+		 * 	  TYPE String: document.querySelector的那个选择器字符串,Eg:'[data-ftc-component]="ftc-header"]'
+		 *    TYPE Element: 直接就是目标header元素，其特征为具有属性data-ftc-component="ftc-header"
 		 */
 
-		//MARK:先处理参数，得到this.header为本html中属性data-o-component为"o-header"的元素
+		// MARK: 处理constructor参数，得到this.header为本html中属性data-ftc-component为"ftc-header"的元素
 		if (!headerEl) {
-			headerEl = document.querySelector('[data-o-component]="o-header"]');
+			headerEl = document.querySelector('[data-ftc-component]="ftc-header"');
 		} else if (typeof headerEl === 'string') {
 			headerEl = document.querySelector(headerEl);
 		}
 
-		//MARK:如果headerEl含有属性'data-o-header--js'，则return
-		if (headerEl.hasAttribute('data-o-header--js')) {
+		//MARK：如果headerEl中已经含有属性'data-ftc-header--js，表明该headerEl已经是具有header相关的js了，故不用再做以下任何工作了。正常还不具备相关js的原始headerEl应该是含有属性'data-ftc-header--no-js'，而不含有'data-ftc-header--js'
+		if (header.hasAttribute('data-ftc-header--js')) {
 			return;
 		}
-
 		this.headerEl = headerEl;
 
-		//MARK:使用该元素（即this.header）初始化其他几个子模块组件
-		search.init(this.headerEl);
-		mega.init(this.headerEl);
-		drawer.init(this.headerEl);
-		subnav.init(this.headerEl);
-		sticky.init(this.headerEl);
+		// TODO:使用this.headerEl初始化其他几个子模块
 
-		//MARK:处理该元素的属性设置，移除'data-o-header--no-js'属性，添加'data-o-header--js'属性
-		this.headerEl.removeAttribute('data-o-header--no-js');
-		this.headerEl.setAttribute('data-o-header--js', '');
+		// MARK:处理该headerEl的属性设置，因为已经添加了修改js功能模块，故移除属性'data-ftc-header--no-js'，添加属性'data-ftc-header--js'
+		this.headerEl.removeAttribute('data-ftc-header--no-js');
+		this.headerEl.setAttribute('data-ftc-header--js','')
 	}
 
 	static init (rootEl) {
 		/**
-		 * @param rootEl:TYPE HTMLElement or String.TYPE HTMLELement, Eg：document.body; TYPE String: the string of document.querySelector
+		 * @param rootEl: TYPE HTMLElement or String
+		 * 	TYPE HTMLElement,要初始化所有属性data-ftc-component为ftc-header的元素的区域的顶级元素，Eg:document.body
+		 *  TYPE String: 以上HTMLElement使用document.querySelector传入的选择器字符串
 		 */
 		if (!rootEl) {
 			rootEl = document.body;
-		}
-		if (!(rootEl instanceof HTMLELement)) {
+		} else if (typeof rootEl === 'string') {
 			rootEl = document.querySelector(rootEl);
 		}
 
-		// MARK:如果rootEl本身含有属性data-o-componen=o-beader，则返回一个 new Header(rootEl)
-		if (/\bo-header\b/.test(rootEl.getAttribute('data-o-component'))) {
-			// KNOWLEDGE:\b 匹配单词边界；\B 匹配非单词边界
+		// MARK:如果rootEl本身含有属性data-ftc-component=ftc-header,则返回一个new Header(rootEl)
+		if (/\bftc-header\b/.test(rootEl.getAttribute('data-ftc-component'))) { //NOTE:用带有零宽单词边界\b的正则匹配，可以包容该属性的写法为data-ftc-component=" ftc-header"/"ftc-header "这种前后有空格的情况。
 			return new Header(rootEl);
 		}
 
-		// MARK: 如果rootEl本身不含属性data-o-component=o-header,但其下的子元素中含有属性'data-o-component = "o-header"'，如果这些子元素不具有属性'data-o-header--js'那么就返回一个new Header(el),这样就得到了一系列的new Header(el),然后过滤处理其中不为undefined的new Header(el)
-		return [].map.call(rootEl.querySelectorAll('[data-o-component="o-header"]'), el => {
-			// KNOWLEDGE: 注意还可以像这样用call的写法来使用数组的map方法
-			if(!el.hasAttribute('data-o-header--js')) {
+		// MARK: 如果rootEl本身不含属性data-ftc-component=ftc-header,但其下的子元素中含有属性'data-ftc-component = "ftc-header"'，如果这些子元素不具有属性'data-ftc-header--js'那么就返回一个new Header(el),这样就得到了一系列的new Header(el),然后过滤掉其中为undefined的new Header(el), 以返回其中不为undefined的new Header(el) —— 这是因为new Header(el)处理已经具有属性'data-ftc-header--js'的元素时，会直接return,那么这个new Header(el)的结果就是undefined
+		return rootEl.querySelectorAll('[data-ftc-component="ftc-header"').map( el => {
+			if (!el.hasAttribute('data-ftc-header--js')) {
 				return new Header(el);
 			}
-		}).filter((header) => {
-			return header !== undefined;//QUET:为什么会出现为undefined的new Header()呢？
-		});
+		}).filter( el => {
+			return header !== undefined;//MARK:
+		})
 	}
 }
 
